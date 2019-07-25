c09938a0cf8c8393e201517ff22fd89600933b2f
Alex Chan <AJChan303@gmail.com>
2019-07-25 10:52:31 -0400

    Done but its still adding the last 1

--- PigDiceAttempt2/Program.cs
+++ PigDiceAttempt2/Program.cs
@@ -6,15 +6,17 @@ namespace PigDiceAttempt2 {
             int[] dice = new int[6] { 1, 2, 3, 4, 5, 6 };
             Random ran = new Random();
             int start = dice[ran.Next(0, dice.Length)];
-            
+           // Console.WriteLine($"Dice Roll: {start}");
             int total = 0;
-            Console.WriteLine($"Dice roll: {start}"); 
+            
 
             while (start != 1) {
                 start = dice[ran.Next(0, dice.Length)];
                 total = total + start;
+                Console.WriteLine($"{start}");
             }
             Console.WriteLine($"Game Total: {total}");
         }
     }
 }
+//highest score 142
