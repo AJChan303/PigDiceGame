5f6805083c5c63236e4d154d197add84d9761895
Alex Chan <AJChan303@gmail.com>
2019-07-25 09:51:05 -0400

    Add project files.

---
+++ PigDiceAttempt2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PigDiceAttempt2 {
+    class Program {
+        static void Main(string[] args) {
+            int[] dice = new int[6] { 1, 2, 3, 4, 5, 6 };
+            Random ran = new Random();
+            int start = dice[ran.Next(0, dice.Length)];
+            
+            int total = 0;
+            Console.WriteLine($"Dice roll: {start}");
+
+            while (start != 1) {
+                start = dice[ran.Next(0, dice.Length)];
+                total = total + start;
+            }
+            Console.WriteLine($"Game Total: {total}");
+        }
+    }
+}
